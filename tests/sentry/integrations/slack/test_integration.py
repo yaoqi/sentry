ddb26f8a9429a4300045624fe4b439d14b564001
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-05-10 16:23:02 -0700

    ref(slack): Implement bot app switch
    Introduces as SLACK_INTEGRATION_USER_WST setting that can be enabled to
    use the legacy workspace token apps (which is what sentry.io uses).
    
     - When installing the integration we now use scopes depending on if
       the workspace token app setting is enabled. Otherwise we use bot
       tokens. NOTE that the WST app scopes have not changed.
    
     - When using bot tokens we must store the bot token _and_ user token to
       handle unfurling.
    
     - We can still pre-link the authorizing user, but we have to make a API
       request first to receive the users ID.

--- tests/sentry/integrations/slack/test_integration.py
+++ tests/sentry/integrations/slack/test_integration.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import responses
 import six
 
+from django.test.utils import override_settings
 from six.moves.urllib.parse import parse_qs, urlencode, urlparse
 
 from sentry.integrations.slack import SlackIntegrationProvider
@@ -10,6 +11,7 @@ from sentry.models import Identity, IdentityProvider, IdentityStatus, Integratio
 from sentry.testutils import IntegrationTestCase
 
 
+@override_settings(SLACK_INTEGRATION_USE_WST=True)
 class SlackIntegrationTest(IntegrationTestCase):
     provider = SlackIntegrationProvider
 
@@ -33,7 +35,7 @@ class SlackIntegrationTest(IntegrationTestCase):
         authorize_params = {k: v[0] for k, v in six.iteritems(params)}
 
         responses.add(
-            responses.POST, 'https://slack.com/api/oauth.token',
+            responses.POST, 'https://slack.com/api/oauth.access',
             json={
                 'ok': True,
                 'access_token': 'xoxp-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx',
@@ -141,3 +143,109 @@ class SlackIntegrationTest(IntegrationTestCase):
         self.assert_setup_flow(authorizing_user_id='UXXXXXXX2')
         identity = Identity.objects.get()
         assert identity.external_id == 'UXXXXXXX2'
+
+
+class SlackIntegrationBotAppTest(IntegrationTestCase):
+    provider = SlackIntegrationProvider
+
+    def assert_setup_flow(self, team_id='TXXXXXXX1', authorizing_user_id='UXXXXXXX1'):
+        responses.reset()
+
+        resp = self.client.get(self.init_path)
+        assert resp.status_code == 302
+        redirect = urlparse(resp['Location'])
+        assert redirect.scheme == 'https'
+        assert redirect.netloc == 'slack.com'
+        assert redirect.path == '/oauth/authorize'
+        params = parse_qs(redirect.query)
+        assert params['scope'] == [' '.join(self.provider.identity_oauth_scopes)]
+        assert params['state']
+        assert params['redirect_uri'] == ['http://testserver/extensions/slack/setup/']
+        assert params['response_type'] == ['code']
+        assert params['client_id'] == ['slack-client-id']
+        # once we've asserted on it, switch to a singular values to make life
+        # easier
+        authorize_params = {k: v[0] for k, v in six.iteritems(params)}
+
+        responses.add(
+            responses.POST, 'https://slack.com/api/oauth.access',
+            json={
+                'ok': True,
+                'access_token': 'xoxa-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx',
+                'bot': {
+                    'bot_access_token': 'xoxb-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx',
+                },
+                'team_id': team_id,
+                'team_name': 'Example',
+                'authorizing_user_id': authorizing_user_id,
+            }
+        )
+
+        responses.add(
+            responses.GET, 'https://slack.com/api/auth.test',
+            json={
+                'ok': True,
+                'user_id': authorizing_user_id,
+            }
+        )
+
+        responses.add(
+            responses.GET, 'https://slack.com/api/team.info',
+            json={
+                'ok': True,
+                'team': {
+                    'domain': 'test-slack-workspace',
+                    'icon': {'image_132': 'http://example.com/ws_icon.jpg'},
+                },
+            }
+        )
+
+        resp = self.client.get(u'{}?{}'.format(
+            self.setup_path,
+            urlencode({
+                'code': 'oauth-code',
+                'state': authorize_params['state'],
+            })
+        ))
+
+        mock_request = responses.calls[0].request
+        req_params = parse_qs(mock_request.body)
+        assert req_params['grant_type'] == ['authorization_code']
+        assert req_params['code'] == ['oauth-code']
+        assert req_params['redirect_uri'] == ['http://testserver/extensions/slack/setup/']
+        assert req_params['client_id'] == ['slack-client-id']
+        assert req_params['client_secret'] == ['slack-client-secret']
+
+        assert resp.status_code == 200
+        self.assertDialogSuccess(resp)
+
+    @responses.activate
+    def test_basic_flow(self):
+        self.assert_setup_flow()
+
+        integration = Integration.objects.get(provider=self.provider.key)
+        assert integration.external_id == 'TXXXXXXX1'
+        assert integration.name == 'Example'
+        assert integration.metadata == {
+            'access_token': 'xoxb-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx',
+            'user_access_token': 'xoxa-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx',
+            'scopes': sorted(self.provider.identity_oauth_scopes),
+            'icon': 'http://example.com/ws_icon.jpg',
+            'domain_name': 'test-slack-workspace.slack.com',
+        }
+        oi = OrganizationIntegration.objects.get(
+            integration=integration,
+            organization=self.organization,
+        )
+        assert oi.config == {}
+
+        idp = IdentityProvider.objects.get(
+            type='slack',
+            external_id='TXXXXXXX1',
+        )
+        identity = Identity.objects.get(
+            idp=idp,
+            user=self.user,
+            external_id='UXXXXXXX1',
+        )
+        assert identity.status == IdentityStatus.VALID
